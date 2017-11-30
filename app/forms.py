a286e6d8e14d0039c1266ce7c17ef3318222a103
Israel Alvarez <ialvarez@nyc01alee.local>
2017-11-30 12:55:04 -0500

    functional toy form script

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,27 @@
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, IntegerField
 from wtforms.validators import DataRequired
 
 class SignUpForm(Form):
     full_name = StringField('full_name', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired()])
     phone_number = IntegerField('phone_number', validators=[DataRequired()])
+
+#######
+'''
+testing
+'''
+#######
+
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField, SubmitField, validators
+
+def CheckNameLength(form, field):
+  if len(field.data) < 4:
+    raise ValidationError('Name must have more then 3 characters')
+
+class ContactForm(Form):
+    name = StringField('Your Name:', [validators.DataRequired(), CheckNameLength])
+    email = StringField('Your e-mail address:', [validators.DataRequired(), validators.Email('your@email.com')])
+    message = TextAreaField('Your message:', [validators.DataRequired()])
+    submit = SubmitField('Send Message')
