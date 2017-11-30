ec15bdbea696e525349b4b8d8d7e0f44ae565480
Israel Alvarez <ialvarez@nyc01alee.local>
2017-11-30 12:56:12 -0500

    functional views for toy contact form

--- app/views.py
+++ app/views.py
@@ -11,3 +11,45 @@ def home():
 def sign_up():
     return render_template('landing_page.html',
                             form=form)
+###########
+'''
+test
+'''
+############
+
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+from forms import ContactForm
+
+
+# app = Flask(__name__)
+app.secret_key = 'YourSuperSecreteKey'
+
+# add mail server config
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'damiank1349@gmail.com'
+app.config['MAIL_PASSWORD'] = 'theexorcist357'
+
+mail = Mail(app)
+
+@app.route('/contact', methods=('GET', 'POST'))
+def contact():
+    form = ContactForm()
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return 'Please fill in all fields <p><a href="/contact">Try Again!!!</a></p>'
+        else:
+            msg = Message("Message from your visitor" + form.name.data,
+                          sender='YourUser@NameHere',
+                          recipients=['yourRecieve@mail.com', 'someOther@mail.com'])
+            msg.body = """
+            From: %s <%s>,
+            %s
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+            return "Successfully  sent message!"
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
